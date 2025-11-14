9772d70a20848d1c004917d5a8bf7e07c90299ea
alvinbengeorge <alvinallen333@gmail.com>
2025-11-14 11:14:32 +0530

    fix: refactor client model to use pydantic

--- model.py
+++ model.py
@@ -1,21 +1,8 @@
 from pydantic import BaseModel
 
-class Client:
-    def __init__(self, name: str, phone_number: str):
-        self.name = name
-        self.phone = phone_number
-
-    def __dict__(self):
-        return {
-            "name": self.name,
-            "phone": self.phone
-        }
-    
-    def __str__(self):
-        return f"""
-NAME: {self.name}
-PHONE: {self.phone}
-"""
+class Client(BaseModel):
+    name: str
+    phone: str
 
 class Ticket(BaseModel):
     name: str
